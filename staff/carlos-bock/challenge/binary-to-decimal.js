88dc71ed747be0be0b51467f3b9c126e0a75892a
CarlosR <carlosrbock@gmail.comgit>
2024-07-02 22:11:23 +0200

    update file for decimal challenge#27

--- staff/carlos-bock/challenge/binary-to-decimal.js
+++ staff/carlos-bock/challenge/binary-to-decimal.js
@@ -1,2 +1,25 @@
 //create a function that changes binary numbers to decimal. 
 
+
+function binaryToDecimal(binary) {
+    var decimal = 0;
+    decimal = parseInt(binary,2);
+    return decimal;
+  }
+  
+  var binary = "1110";
+  
+  console.log(binary + " en decimal es: " + binaryToDecimal(binary));
+  
+
+
+  /* creating an alternative function to convert the numbers
+  function binaryConvertDec(binarynum) {
+    var decimal = 0;
+
+    return decimal;
+  }
+  
+  var binarynum = "1110";
+  
+  console.log(binarynum + " en decimal es: " + binaryConvertDec(binarynum)); */
