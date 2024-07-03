14361f1205e67b2d2204a191064f2a5ac14c3709
LeideVanessaC <lei.vanessacardoso@gmail.com>
2024-07-03 20:14:13 +0200

    correct algoritmo the binary-to-decimal.js #36

--- staff/vanessa-cardoso/challenges/binay-to-decimal.js
+++ staff/vanessa-cardoso/challenges/binay-to-decimal.js
@@ -1,10 +1,17 @@
 // crear una function de conversión em binario
 
-function binaryToDecimal(binario) {
+function binaryToDecimal(binary) {
     var decimal = 0;
+
+    for ( i = 0; i < binary.length; i++ ) {
+        var digit = binary[binary.length - 1 - i];
+
+        decimal += digit * Math.pow(2, i)
+
+    }
     return decimal;
-}
+  }
 
-var binary = "11111";
+  var binary = "6010";
 
-console.log(binary + "en decimal es: " + binaryToDecimal(binary));
+  console.log(binary + " en decimal es: " + binaryToDecimal(binary));
