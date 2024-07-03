742b158383684ff0acaca5d6f4a6681fa6280e97
LeideVanessaC <lei.vanessacardoso@gmail.com>
2024-07-03 17:43:42 +0200

    add challenges structure #36

---
+++ staff/vanessa-cardoso/challenges/binay-to-decimal.js
@@ -0,0 +1,10 @@
+// crear una function de conversión em binario
+
+function binaryToDecimal(binario) {
+    var decimal = 0;
+    return decimal;
+}
+
+var binary = "11111";
+
+console.log(binary + "en decimal es: " + binaryToDecimal(binary));
