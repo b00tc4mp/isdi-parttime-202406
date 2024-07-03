473bfffc348f0679e659eb6e895e1ecb45647b95
xFNighTMaRe <brodriguezserrano54@gmail.com>
2024-07-03 21:31:05 +0200

    add challenge counting letters #44

---
+++ staff/ventura-rodriguez/challenges/counting-letters.js
@@ -0,0 +1,21 @@
+// Crea una funcion llamada 'countingLetters' que admita un parámetro de tipo
+// string y te devuelva el largo de la palabra. Ej: 'hola' ==> 4.
+// NO CHAT GPT
+
+function countingLetters(value) {
+  var result;
+
+  return result;
+}
+
+var result1 = countingLetters("hola");
+console.assert(result1 === 4, { result: result1, message: "Test 1 no pasado" });
+
+var result2 = countingLetters("belen");
+console.assert(result2 === 5, { result: result2, message: "Test 2 no pasado" });
+
+var result3 = countingLetters("foo");
+console.assert(typeof result3 === "number", {
+  result: result3,
+  message: "Test 3 no pasado",
+});
