2e53a0dcdbf907b910257a697d4cedef117f7fbf
Rafa0297 <158144592+Rafa0297@users.noreply.github.com>
2024-07-04 20:12:30 +0200

    get back to previous TDD version #50

--- staff/rafael-infante/challenges/counting-letters.js
+++ staff/rafael-infante/challenges/counting-letters.js
@@ -11,9 +11,7 @@ console.assert(result1 === 4, { result: result1, message: "Test 1 no pasado" });
 var result2 = countingLetters("belen");
 console.assert(result2 === 5, { result: result2, message: "Test 2 no pasado" });
 var result3 = countingLetters("foo");
-console.assert(typeof result3 === "number" && result3 === 3, {
+console.assert(typeof result3 === "number", {
   result: result3,
   message: "Test 3 no pasado",
 });
-
-// Le he agregado '&& result3 === 3' al último console.assert()
