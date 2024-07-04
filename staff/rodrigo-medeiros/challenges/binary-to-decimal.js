6417726ef76c19341fec515b03a9c838ef0b99d7
rodrigo-medeiros <gapulflu83@gmail.com>
2024-07-04 16:00:56 -0300

    create-binary-to-decimal #59

---
+++ staff/rodrigo-medeiros/challenges/binary-to-decimal.js
@@ -0,0 +1,12 @@
+// Genera una función de conversión de binario a decimal. El valor pasado por parámetro debe
+// ser una cadena de caracteres de 0 y 1. Imprime el resultado por pantalla.
+
+function binaryToDecimal(binary) {
+    var decimal = 0;
+  
+    return decimal;
+  }
+  
+  var binary = "1110";
+  
+  console.log(binary + " en decimal es: " + binaryToDecimal(binary));
