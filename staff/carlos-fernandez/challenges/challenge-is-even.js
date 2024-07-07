1c7f522f7ac6be986b228d2f4f9252675972460c
CarlosF <carlosfernandezsoldado@gmail.com>
2024-07-07 12:23:05 +0200

    testing-challenge-is-even-#69

---
+++ staff/carlos-fernandez/challenges/challenge-is-even.js
@@ -0,0 +1,41 @@
+
+// 1. Crea una función que devuelva true o false si el número
+// pasado por parámetro es par o impar. Crea los test correspondientes.
+
+debugger
+{
+function isEven(value) {
+    if (typeof value === "number") {
+        return value % 2 === 0;
+    } else if (typeof value === "string") {
+        return value.length % 2 === 0;
+    } else {
+        return false;
+    }
+}
+/* he usado typeof porque al llegar a las strings no me funcionaba. De esta manera 
+también podemos jugar con las strings */
+
+let result1 = isEven(1)
+console.assert(result1 === false, {result: result1, message: 'Test 1 no pasado'})
+
+let result2 = isEven(2)
+console.assert(result2 === true, {result: result2, message: 'Test 2 no pasado'})
+
+let result3 = isEven(0)
+console.assert(result3 === true, {result: result3, message: 'Test 3 no pasado'})
+
+let result4 = isEven('hola')
+console.assert(result4 === true, {result: result4, message: 'Test 4 no pasado'})
+
+let result5 = isEven('adios')
+console.assert(result5 === false, {result: result5, message: 'Test 5 no pasado'})
+
+let result6 = isEven(Number.MAX_SAFE_INTEGER)
+console.assert(result6 === false, {result: result6, message: 'Test 6 no pasado'})
+
+let result7 = isEven(Number.MAX_SAFE_INTEGER + 1)
+console.assert(result7 === true, {result: result7, message: 'Test 7 no pasado'})
+
+}
+//con las condiciones actuales, pasan todos los tests 
