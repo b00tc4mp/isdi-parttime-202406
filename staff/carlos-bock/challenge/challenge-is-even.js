d69f234b40a3db043d35b2c819197117d65bb5cf
carlos <carlosrbock@gmail.comgit>
2024-07-07 10:31:12 +0200

    add testing 3rd commit #64

--- staff/carlos-bock/challenge/challenge-is-even.js
+++ staff/carlos-bock/challenge/challenge-is-even.js
@@ -1,8 +1,8 @@
 // 1. Crea una función que devuelva true o false si el número
 // pasado por parámetro es par o impar. Crea los test correspondientes.
 
-//debugger
-//{
+debugger
+{
 function isEven(value) {
     var result
     if (typeof value !== 'number' && typeof value !== 'string') {
@@ -48,17 +48,20 @@ isEven(2);
 isEven(3);
 isEven("failure");
 isEven([1,2,"pie"]);
+isEven(Number.MAX_SAFE_INTEGER) //test fails which is correct
 isEven(Number.MAX_SAFE_INTEGER + 2); // test is passing, likely unpredictable behavior in JS due to max integer
 
-/*let result1 = isEven(1)
+console.log("line break for customer testing------------");
+
+let result1 = isEven(1)
 isEven(1);
 console.assert(result1 === false, {result: result1, message: 'Test 1 no pasado'})  
 // I think the client's testing parameters, result1 is not matching the result variable
 
 let result2 = isEven(2)
-console.assert(result2 === true, {result: result2, message: 'Test 2 ha pasado'}) */
+console.assert(result2 === true, {result: result2, message: 'Test 2 ha pasado'})
 
-/*let result3 = isEven(0)
+let result3 = isEven(0)
 console.assert(result3 === true, {result: result3, message: 'Test 3 no pasado'})
 
 let result4 = isEven('hola')
@@ -71,8 +74,8 @@ let result6 = isEven(Number.MAX_SAFE_INTEGER)
 console.assert(result6 === false, {result: result6, message: 'Test 6 no pasado'})
 
 let result7 = isEven(Number.MAX_SAFE_INTEGER + 1)
-console.assert(result7 === true, {result: result7, message: 'Test 7 no pasado'})*/
+console.assert(result7 === true, {result: result7, message: 'Test 7 no pasado'})
 
-//}
+}
 
 
