8bb09b0d6b10e9f92ef297823fd3d7a973f3fab9
rodrigo-medeiros <gapulflu83@gmail.com>
2024-07-10 11:55:00 -0300

    insertion of correct coding

--- staff/rodrigo-medeiros/challenges/binary-to-decimal.js
+++ staff/rodrigo-medeiros/challenges/binary-to-decimal.js
@@ -2,11 +2,21 @@
 // ser una cadena de caracteres de 0 y 1. Imprime el resultado por pantalla.
 
 function binaryToDecimal(binary) {
-    var decimal = 0;
-  
-    return decimal;
+  let decimal = 0;
+  let longitud = binary.length;
+
+  for (let i = 0; i < longitud; i++) {
+      // dígito actual
+      let digito = binary[longitud - 1 - i];
+      // Convertir el dígito a número (debe ser 0 o 1)
+      if (digito === '1') {
+          // Sumar la potencia de 2 correspondiente al resultado final
+          decimal += Math.pow(2, i);
+      }
   }
-  
-  var binary = "1110";
-  
-  console.log(binary + " en decimal es: " + binaryToDecimal(binary));
+
+  return decimal;
+}
+var binary = "1110";
+
+console.log(binary + " en decimal es: " + binaryToDecimal(binary));
