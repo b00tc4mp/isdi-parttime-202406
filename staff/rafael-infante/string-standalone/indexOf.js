bf5c935d8d9f012744635b09adadbbc6bff22f7a
Rafa0297 <158144592+Rafa0297@users.noreply.github.com>
2024-07-13 23:20:09 +0200

    complete indexOf code #96

---
+++ staff/rafael-infante/string-standalone/indexOf.js
@@ -0,0 +1,44 @@
+function indexOf(string, searchTerm) {
+
+  if (searchTerm === null) return -1;
+  if (searchTerm === '') return 0;
+  
+  
+  
+  let result = 0
+  let count = 0
+  for (let i = 0; i < string.length; i++) {
+      
+      for (let j = 0; j < searchTerm.length; j++){
+          let characterOfString = string[i + j];
+          let characterOfSearchTerm = searchTerm[j];
+          
+
+          if (characterOfString === characterOfSearchTerm){
+              count++
+              if (count === searchTerm.length) {
+                  
+                  return i
+              } 
+              
+          }
+  
+      }
+  }
+  
+  
+}
+
+const result1 = indexOf("Hello, my name is Carl", "l");
+console.assert(result1 === "Hello, my name is Carl".indexOf("l"), { result: result1, message: "Test 1 No pasado ",});
+
+const result2 = indexOf("Hello, my name is Carl", "my");
+console.assert(result2 === "Hello, my name is Carl".indexOf("my"), { result: result2, message: "Test 2 No pasado ",});
+
+const result3 = indexOf("Lamine Yamal cada dia te quiero más", "");
+console.assert(result3 === "Lamine Yamal cada dia te quiero más".indexOf(""), { result: result3, message: "Test 3 No pasado ",});
+
+const result4 = indexOf("Chiquito de la calzada", null);
+console.assert(result4 === "Chiquito de la calzada".indexOf(null), { result: result4, message: "Test 4 No pasado ",});
+
+
