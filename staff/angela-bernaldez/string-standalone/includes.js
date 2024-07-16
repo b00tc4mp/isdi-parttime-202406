399baff85eb242376ad1f7b7cc75a007ff4063a6
Angela Bernaldez <angelabernaldz@gmail.com>
2024-07-16 19:30:55 +0100

    add includes challenge solved #105

--- staff/angela-bernaldez/string-standalone/includes.js
+++ staff/angela-bernaldez/string-standalone/includes.js
@@ -1,66 +1,63 @@
 //str.includes(searchString)
 //Version stand alone
-
-
 function includes(string, searchString) {
-    // tenemos 2 cadenas de caracteres y tenemos que mirar si la segunda cadena de caracteres
-    // está incluida en la primer cadena de caracteres
-  
-    // "hola" te pregunto: "la" está incluida en la palabra "hola"
-    // Coges la palabra que te han dado y buscas recorriendo si el primer caracter de la segunda palabra
-    // coincide con alguno de las caracteres de la primera palabra. en caso de que si
-    // buscas si el segundo caracter de la segunda palabra machea con el caracter consecutivo
-    // al caracter encontrado en la primera palabra
-    // si todo machea, entonces devuelvo que ha ido bien, si no digo que no ha ido bien
-  
-    if (searchString === null) return false;
-  
-    let result = false;
-  
-    for (let i = 0; i < string.length; i++) {
-  
-          let count = 0 
-          for (let j=0; j<searchString.length; j++) {
-  
-              const characterOfString = string[i]
-              const characterOfSearchString = searchString[j]
-  
-              if (characterOfString === characterOfSearchString) {
-                  count++
-                  if (count === searchString.length) {
-                      return true 
-                  }
-                  i++
-              }
-              else {
-                  count = 0
-                  result = false 
-              }
-          }
+  if (searchString === null) return false;
+
+  let result = false;
+
+  for (let i = 0; i < string.length; i++) {
+    let countMatches = 0;
+
+    for (let j = 0; j < searchString.length; j++) {
+      if (string[i + j] === searchString[j]) countMatches++;
     }
-    return result;
+
+    if (countMatches === searchString.length) result = true;
   }
-  
-  const result1 = includes("Hola", "H");
-  console.assert(result1 ==="Hello world".includes("H"), {
-    result: result1,
-    message: "Test 1 No pasado ",
-  });
-  
-  const result2 = includes("Hola", "Ho");
-  console.assert(result2 === "Hola".includes("Ho"), {
-    result: result2,
-    message: "Test 2 No pasado ",
-  });
-  
-  const result3 = includes("casoSinIndice", null);
-  console.assert(result3 === "casoSinIndice".includes(null), {
-    result: result3,
-    message: "Test 3 No pasado ",
-  });
-  
-  const result4 = includes("world", "wr");
-  console.assert(result4 === "world".includes("wr"), {
-    result: result4,
-    message: "Test 4 No pasado ",
-  });
+
+  return result;
+}
+
+const result1 = includes("Hello", "H");
+console.assert(result1 === "Hello".includes("H"), {
+result: result1,
+message: "Test 1 No pasado ",
+});
+
+const result2 = includes("Hola", "Ho");
+console.assert(result2 === "Hola".includes("Ho"), {
+result: result2,
+message: "Test 2 No pasado ",
+});
+
+const result3 = includes("casoSinIndice", null);
+console.assert(result3 === "casoSinIndice".includes(null), {
+result: result3,
+message: "Test 3 No pasado ",
+});
+
+
+
+const result4 = includes("world", "wr");
+console.assert(result4 === "world".includes("wr"), {
+result: result4,
+message: "Test 4 No pasado ",
+});
+
+const result5 = includes("world", "rld");
+console.assert(result5 === "world".includes("rld"), {
+result: result5,
+message: "Test 5 No pasado ",
+});
+
+const result6 = includes("world", "wod");
+console.assert(result6 === "world".includes("wod"), {
+result: result6,
+message: "Test 6 No pasado ",
+});
+
+const result7 = includes("world", "wu");
+console.assert(result7 === "wu".includes("wod"), {
+result: result7,
+message: "Test 7 No pasado ",
+});
