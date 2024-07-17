62a8a6ef42a49cbeb5e78e15409ff32dba735a0c
LeideVanessaC <lei.vanessacardoso@gmail.com>
2024-07-17 10:23:50 +0200

    updating variables to const #75

--- staff/vanessa-cardoso/challenges/counting-letters.js
+++ staff/vanessa-cardoso/challenges/counting-letters.js
@@ -7,11 +7,11 @@ function countingLetters(value) {
     
 }
 
-var result1 = countingLetters('hola')
+const result1 = countingLetters('hola')
 console.assert(result1 === 4, {result: result1, message: 'test 1 no pasado'})
 
-var result2 = countingLetters('belen')
+const result2 = countingLetters('belen')
 console.assert(result2 === 5, {result: result2, message: 'test 2 no pasado'})
 
-var result3 = countingLetters('foo')
+const result3 = countingLetters('foo')
 console.assert(typeof result3 === 'number', {result: result3, message: 'test 3 no pasado'})
