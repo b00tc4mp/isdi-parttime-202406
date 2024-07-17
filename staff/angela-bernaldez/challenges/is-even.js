5602e06a8f1e3ebbcd6c858a19b236a903f3b2a7
Angela Bernaldez <angelabernaldz@gmail.com>
2024-07-17 17:24:30 +0100

    add is even challenge solution mofied #91

--- staff/angela-bernaldez/challenges/is-even.js
+++ staff/angela-bernaldez/challenges/is-even.js
@@ -9,23 +9,23 @@ function isEven(value) {
   return result
 }
 
-let result1 = isEven(1)
+const result1 = isEven(1)
 console.assert(result1 === false, {result: result1, message: 'Test 1 no pasado'})
 
-let result2 = isEven(2)
+const result2 = isEven(2)
 console.assert(result2 === true, {result: result2, message: 'Test 2 no pasado'})
 
-let result3 = isEven(0)
+const result3 = isEven(0)
 console.assert(result3 === true, {result: result3, message: 'Test 3 no pasado'})
 
-let result4 = isEven('hola')
+const result4 = isEven('hola')
 console.assert(result4 === true, {result: result4, message: 'Test 4 no pasado'})
 
-let result5 = isEven('adios')
+const result5 = isEven('adios')
 console.assert(result5 === false, {result: result5, message: 'Test 5 no pasado'})
 
-let result6 = isEven(Number.MAX_SAFE_INTEGER)
+const result6 = isEven(Number.MAX_SAFE_INTEGER)
 console.assert(result6 === false, {result: result6, message: 'Test 6 no pasado'})
 
-let result7 = isEven(Number.MAX_SAFE_INTEGER + 1)
+const result7 = isEven(Number.MAX_SAFE_INTEGER + 1)
 console.assert(result7 === true, {result: result7, message: 'Test 7 no pasado'})
