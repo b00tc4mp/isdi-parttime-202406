db88f04da4ec38cc04055fe73999fbcba8b773cc
Angela Bernaldez <angelabernaldz@gmail.com>
2024-07-19 17:20:14 +0100

    add ends with challenge solution #105

--- staff/angela-bernaldez/string-standalone/ends-with.js
+++ staff/angela-bernaldez/string-standalone/ends-with.js
@@ -1,34 +1,57 @@
 //str.endsWith(searchString, endPosition)
 //Version stand alone!
-function endsWith(string, searchString, endPosition = searchString.length) {
+function endsWith(string, searchString, endPosition = string.length) {
 
-    // returns true or false
-    // returns true if searchString is found at the end of the string
-    // returns true when searchString is an empty string
-    // returns false otherwise
-
-    let result 
-
-    if (searchString === "") result = true
+    if (typeof searchString !== 'string') return false
+    else if (searchString === "") return true     
+    
+    let result = true 
 
     for (let i = 0; i < searchString.length; i++) {
-        if (string[string.length - searchString.length + i] !== searchString[i]) result = false
-        else result = true
+        console.log(string[endPosition - searchString.length + i])
+        console.log(searchString[i])
+        if (string[endPosition - searchString.length + i] !== searchString[i]) { 
+            result = false
+            break 
+        }
     }
-
-    return result 
+    return result   
 }
 
 
-
+ 
 const result1 = endsWith("Hello world, this is me", "me");
-console.assert(result1 === "Hello world, this is me".concat("me"), {
+console.assert(result1 === "Hello world, this is me".endsWith("me"), {
   result: result1,
   message: "Test 1 No pasado ",
 });
 
-const result2 = concat("Hello world, this is me", "me", 22);
-console.assert(result2 === "Hello".concat(" ", "world"), {
+const result2 = endsWith("Hello world, this is me", "world", 11);
+console.assert(result2 === "Hello world, this is me".endsWith("world", 11), {
   result: result2,
   message: "Test 2 No pasado ",
+});
+
+const result3 = endsWith("Hello world, this is me", "world", 14);
+console.assert(result3 === "Hello world, this is me".endsWith("world", 14), {
+  result: result3,
+  message: "Test 3 No pasado ",
+});
+
+const result4 = endsWith("My name is Angela", undefined);
+console.assert(result4 === "My name is Angela".endsWith(undefined), {
+  result: result4,
+  message: "Test 4 No pasado ",
+});
+
+const result5 = endsWith("My name is Angela", "");
+console.assert(result5 === "My name is Angela".endsWith(""), {
+  result: result5,
+  message: "Test 5 No pasado ",
+});
+
+const result6 = endsWith("My name is Angela", null);
+console.assert(result6 === "My name is Angela".endsWith(null), {
+  result: result6,
+  message: "Test 6 No pasado ",
 });
