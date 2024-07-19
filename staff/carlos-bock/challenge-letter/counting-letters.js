27a07848f30d726755b64f58cccc126e52cdcfa0
carlos <carlosrbock@gmail.comgit>
2024-07-19 11:31:08 +0200

    delete extra console.log #48

--- staff/carlos-bock/challenge-letter/counting-letters.js
+++ staff/carlos-bock/challenge-letter/counting-letters.js
@@ -3,23 +3,22 @@
 // NO CHAT GPT
 
 function countingLetters(value) {
-    var result;
-    result = value.length;
-    console.log(result);
-    return result;
-  }
-  
-  var result1 = countingLetters("hola");
-  console.assert(result1 === 4, { result: result1, message: "Test 1 no pasado" });
-  
-  var result2 = countingLetters("belen");
-  console.assert(result2 === 5, { result: result2, message: "Test 2 no pasado" });
-  
-  var result3 = countingLetters("foo");
-  console.assert(typeof result3 === "number", {
-    result: result3,
-    message: "Test 3 no pasado",
-  });
+  var result;
+  result = value.length;
+  return result;
+}
 
-  countingLetters("string1");
-  countingLetters("this is a longer string");
+var result1 = countingLetters("hola");
+console.assert(result1 === 4, { result: result1, message: "Test 1 no pasado" });
+
+var result2 = countingLetters("belen");
+console.assert(result2 === 5, { result: result2, message: "Test 2 no pasado" });
+
+var result3 = countingLetters("foo");
+console.assert(typeof result3 === "number", {
+  result: result3,
+  message: "Test 3 no pasado",
+});
+
+countingLetters("string1");
+countingLetters("this is a longer string");
