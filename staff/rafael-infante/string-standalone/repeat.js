c23fccd11362d70df27cd733057b4b1c8235a7d7
Rafa0297 <158144592+Rafa0297@users.noreply.github.com>
2024-07-21 21:12:23 +0200

    add repeat method #96

---
+++ staff/rafael-infante/string-standalone/repeat.js
@@ -0,0 +1,21 @@
+/* recibo dos parametros: un string y un numero. 
+Debo crear una funcion que repita el string tantas veces como me lo indica el numero */
+
+function repeat(str, number) {
+  let newPhrase = '';
+
+  for (let i = 0; i < number; i++){
+      newPhrase += str
+  }
+  return newPhrase
+}
+
+const result1 = repeat('hola', 3)
+console.assert(result1 === 'hola'.repeat(3), {result: result1, message: "Test 1 No pasado "})
+
+const result2 = repeat('hola', 0)
+console.assert(result2 === 'hola'.repeat(0), {result: result2, message: "Test 2 No pasado "})
+
+const result3 = repeat('hola', 1)
+console.assert(result3 === 'hola'.repeat(1), {result: result3, message: "Test 3 No pasado "})
+
