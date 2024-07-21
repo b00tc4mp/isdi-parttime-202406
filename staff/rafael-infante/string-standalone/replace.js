d1b1469bd11e13bad91f25561db20e2e163d340e
Rafa0297 <158144592+Rafa0297@users.noreply.github.com>
2024-07-21 19:47:20 +0200

    modify replace & slice methods #96

--- staff/rafael-infante/string-standalone/replace.js
+++ staff/rafael-infante/string-standalone/replace.js
@@ -1,36 +1,42 @@
+function replace(str, pattern, replacement){
   let i = 0 
   let j = 0 
   let newString = ""; 
   let patternFound = "";
-  let found = false; 
-  let replacement = "replacement" + " "
-function replace(str, pattern, replacement){
+  let found = false;
+  
+      while (i < str.length) {
+  //primer bloque de codigo de busqueda 
+      if( pattern[j] === str[i] ){
+          
+          // aqui aun estoy incluyendo el patron :( -- patron detected position = newstring.length - pattern.length 
+          patternFound += pattern[j];
+       j++;
+           // si coinciden caracteres entre patron y string se guarda en patron trobat =D
+            // y se continua con el siguiente caracter del patron
+        } else {
+            newString += str[i] // cuando no se cumpla la igualdad
+            patternFound = '';// la cadena vuelve a estar vacia
+            j = 0;// empezamos de nuevo con el patron a buscar
+        }
 
-    while (i<str.length) {
-    //primer bloque de codigo de busqueda 
-        if( pattern[j] === str[i] ){
-            
-            // aqui aun estoy incluyendo el patron :( -- patron detected position = newstring.length - pattern.length 
-            patternFound += pattern[j];
-         j++;
-             // si coinciden caracteres entre patron y string se guarda en patron trobat =D
-              // y se continua con el siguiente caracter del patron
-          } else {
-              newString += str[i] // cuando no se cumpla la igualdad
-              patternFound = '';// la cadena vuelve a estar vacia
-              j = 0;// empezamos de nuevo con el patron a buscar
-          }
+        if (patternFound === pattern) { // esto nos sirve para salir de while 
+            found = true;
+            newString += replacement// si se cumple que encontramos el patron el valor found para a true
+            ;
+        }
 
-          if (patternFound === pattern) { // esto nos sirve para salir de while 
-              found = true;
-              newString += replacement// si se cumple que encontramos el patron el valor found para a true
-              ;
-          }
- 
-          i++;// seguimos recorriendo la string 
-      }
-    return newString;// devolvemos la cadena con los remplazos 
-  }
+        i++;// seguimos recorriendo la string 
+    }
+  return newString;// devolvemos la cadena con los remplazos 
+}
 
-const result1 = replace('jnkjnkj palabra njjnk','palabra','replacement')
-console.assert(result1 === 'jnkjnkj palabra njjnk'.replace('palabra', 'replacement'), {result: result1, message: "Test 1 No pasado "})
+const result1 = replace('jnkjnkj palabra njjnk','palabra','word')
+console.assert(result1 === 'jnkjnkj palabra njjnk'.replace('palabra', 'word'), {result: result1, message: "Test 1 No pasado "});
+
+const result2 = replace('jnkjnkj palabra njjnk','palabra',' ')
+console.assert(result2 === 'jnkjnkj palabra njjnk'.replace('palabra', ' '), {result: result2, message: "Test 2 No pasado "});
+
+const result3 = replace('jouihuoh palabra njjnk','way','reemplazo')
+console.assert(result3 === 'jouihuoh palabra njjnk'.replace('way', 'reemplazo'), {result: result3, message: "Test 3 No pasado "});
+
