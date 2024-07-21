0a469ff74828ae2bf9fe7d21bec967f686cce2ed
Rafa0297 <158144592+Rafa0297@users.noreply.github.com>
2024-07-21 20:53:04 +0200

    add to-lowercase method #96

---
+++ staff/rafael-infante/string-standalone/to-lowercase.js
@@ -0,0 +1,33 @@
+/* recibo una cadena de caracteres y debo realizar una funcion que cambie de
+mayuscula a minuscula
+- Guardo en una variable las letras mayusculas y en otra las minusculas.
+- declaro una variable donde guardar lo que voy a devolver
+- recorro el string para saber que letras voy a convertir a minuscula
+*/
+
+function toLowerCase(string) {
+    
+  const mayusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  const minusculas = 'abcdefghijklmnopqrstuvwxyz'
+  let inLowers = ''
+      
+      for (let i = 0; i < string.length; i++) {
+          for (let j = 0; j < mayusculas.length; j++){
+              if (string[i] === mayusculas[j] || minusculas.indexOf(string[i]) === j){
+              inLowers += minusculas[j];
+                  break
+              } 
+          }
+          
+      }
+      return inLowers
+  }
+  
+  const result1 = toLowerCase('HOLA');
+  console.assert(result1 === 'HOLA'.toLowerCase(), { result: result1, message: "Test 1 No pasado ",});
+  
+  const result2 = toLowerCase('hOLA');
+  console.assert(result2 === 'hOLA'.toLowerCase(), { result: result2, message: "Test 2 No pasado ",});
+  
+  const result3 = toLowerCase('HOlaaa');
+  console.assert(result3 === 'HOlaaa'.toLowerCase(), { result: result3, message: "Test 3 No pasado ",});
