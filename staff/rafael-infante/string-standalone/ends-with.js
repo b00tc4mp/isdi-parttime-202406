2409b4e96cfebb5db07b55fd3302aacd7556b30a
Rafa0297 <158144592+Rafa0297@users.noreply.github.com>
2024-07-22 00:09:57 +0200

    add ends-with method #96

---
+++ staff/rafael-infante/string-standalone/ends-with.js
@@ -0,0 +1,35 @@
+/* recibo un string y un patron, si ese string finaliza con ese patron
+se devuelve true*/
+
+function endsWith(string, pattern) {
+  let result = true;
+  if (pattern === null) return false;
+  
+  let j = pattern.length - 1;
+
+  for (let i = string.length - 1; j >= 0; i--) {
+      if (pattern[j] !== string[i]) {
+          result = false;
+          break;
+      }
+      j--
+  }
+  return result
+  }
+
+endsWith('holamundo', 'mundo')
+
+const result1 = endsWith('holamundo', 'mundo')
+console.assert(result1 === 'holamundo'.endsWith('mundo'), {result: result1, message: "Test 1 No pasado "})
+
+const result2 = endsWith('hola Rafa', 'Rafa')
+console.assert(result2 === 'hola Rafa'.endsWith('Rafa'), {result: result2, message: "Test 2 No pasado "})
+
+const result3 = endsWith('holamundo', null)
+console.assert(result3 === 'holamundo'.endsWith(null), {result: result3, message: "Test 3 No pasado "})
+
+
+
+
+
+
