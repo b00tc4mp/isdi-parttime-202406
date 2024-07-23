8ea5e9aadb68a89c70a89d0dbff2a3ee4ef7b78a
CarlosF <carlosfernandezsoldado@gmail.com>
2024-07-23 18:27:03 +0200

    fixing .concat() #107

--- staff/carlos-fernandez/string-standalone/concat.js
+++ staff/carlos-fernandez/string-standalone/concat.js
@@ -2,5 +2,19 @@
 // str.concat()
 
 function concat(...string) {
-    return string.join(' ');
+
+    let result = ''
+    for (let i=0; i < string.length; i++) {
+        result += string[i]
+    }
+    return result 
 };
+
+const result1 = concat('Emisario ', 'gorgonita');
+console.assert(result1 === 'Emisario '.concat('gorgonita'), { result: result1, message: 'Test 1 no pasado' });
+
+const result2 = concat('Ash ', 'Ketchum');
+console.assert(result2 === 'Ash '.concat('Ketchum'), { result: result2, message: 'Test 2 no pasado' });
+
+const result3 = concat('Pueblo', 'Paleta');
+console.assert(result3 === 'Pueblo'.concat('Paleta'), { result: result3, message: 'Test 3 no pasado' });
