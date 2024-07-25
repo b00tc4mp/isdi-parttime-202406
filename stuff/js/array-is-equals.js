d0dc809073fcbfd025807bceb102425f04dac7d0
xFNighTMaRe <brodriguezserrano54@gmail.com>
2024-07-25 21:21:25 +0200

    add array is equals #115

---
+++ stuff/js/array-is-equals.js
@@ -0,0 +1,13 @@
+function arrayIsEqual(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+
+  return true;
+}
