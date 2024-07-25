940892862f3807285c8b344364d719f35189fcaa
xFNighTMaRe <brodriguezserrano54@gmail.com>
2024-07-25 21:23:03 +0200

    update array-is-equals #115

--- stuff/js/array-is-equals.js
+++ stuff/js/array-is-equals.js
@@ -1,13 +1,12 @@
 function arrayIsEqual(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-
-  for (let i = 0; i < arr1.length; i++) {
+  if (arr1.length !== arr2.length) return false;
+  let result = true;
+  let i = 0;
+  while (i < arr1.length || result === false) {
     if (arr1[i] !== arr2[i]) {
-      return false;
+      result = false;
     }
+    i++;
   }
-
-  return true;
+  return result;
 }
