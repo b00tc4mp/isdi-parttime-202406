5ea6a4c795b72ca1e45319e504056ece7bb70bca
Rafa0297 <158144592+Rafa0297@users.noreply.github.com>
2024-07-25 18:49:44 +0200

    modify repeat method

--- staff/rafael-infante/string-standalone/repeat.js
+++ staff/rafael-infante/string-standalone/repeat.js
@@ -11,11 +11,20 @@ function repeat(str, number) {
 }
 
 const result1 = repeat('hola', 3)
-console.assert(result1 === 'hola'.repeat(3), {result: result1, message: "Test 1 No pasado "})
+console.assert(result1 === 'hola'.repeat(3), {result: result1, message: "Test 1 No pasado "});
+if(result1 === 'hola'.repeat(3)) {
+  console.log("Test 1 Pasado")
+}
 
 const result2 = repeat('hola', 0)
-console.assert(result2 === 'hola'.repeat(0), {result: result2, message: "Test 2 No pasado "})
+console.assert(result2 === 'hola'.repeat(0), {result: result2, message: "Test 2 No pasado "});
+if(result2 === 'hola'.repeat(0)) {
+  console.log("Test 2 Pasado")
+}
 
 const result3 = repeat('hola', 1)
-console.assert(result3 === 'hola'.repeat(1), {result: result3, message: "Test 3 No pasado "})
+console.assert(result3 === 'hola'.repeat(1), {result: result3, message: "Test 3 No pasado "});
+if(result3 === 'hola'.repeat(1)) {
+  console.log("Test 3 Pasado")
+}
 
