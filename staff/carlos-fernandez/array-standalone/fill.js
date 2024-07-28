cceff8ac3ddd9d39c51524856307b5aa32c78f6e
CarlosF <carlosfernandezsoldado@gmail.com>
2024-07-28 19:07:59 +0200

    starting fill method #141

---
+++ staff/carlos-fernandez/array-standalone/fill.js
@@ -0,0 +1,15 @@
+/* 
+
+    En un array, cambia unos elementos por otros. Dándole nosotros la posición inicial, la final y cuál va a ser el elemento que va a 
+    substituir a los demás. 
+    
+    Parámetros:
+
+        1) VALUE = valor con el que se va a rellenar el array.
+        2) START = primera posición que va a ser substituida. Por defecto es 0.
+        3) END = última posición que va a ser substituida. Por defecto es array.length.
+
+    */
+
+const result1 = arr,fill(1);
+console.assert()
