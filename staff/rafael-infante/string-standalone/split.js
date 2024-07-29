019c28815ed78c4efc78ea2892d988df2453bb73
Rafa0297 <158144592+Rafa0297@users.noreply.github.com>
2024-07-29 18:38:14 +0200

    finish split method #96

--- staff/rafael-infante/string-standalone/split.js
+++ staff/rafael-infante/string-standalone/split.js
@@ -3,12 +3,25 @@ si encuentro el patron lo elimino y divido la cadena en un array dependiendo de 
 y despues del patron */
 
 function split(string, pattern) {
-  let result = ['hello', 'world']
-  
-  return result
-}
+  let newArray = [];
+  let elementOfArray = '';
 
-console.log(split('The quick brown', ' '))
+  for (let i = 0; i < string.length; i++) {
+  
+    if (pattern === '') {
+      newArray[i] = string[i]; // separo por caracteres
+
+    } else if (string[i] === pattern) {
+      newArray[newArray.length] = elementOfArray;
+      elementOfArray = '';
+
+    } else {
+      elementOfArray += string[i];
+    }
+    
+  }
+  return newArray
+}
 
 function arrayIsEqual(arr1, arr2) {
   if (arr1.length !== arr2.length) return false;
@@ -24,14 +37,23 @@ function arrayIsEqual(arr1, arr2) {
 }
 
 
-const result1 = split('hello world', ' ')
-console.assert(arrayIsEqual(result1, ['hello', 'world']), {result: result1, message: "Test 1 No pasado "})
+const result1 = split('hello world', '')
+console.assert(arrayIsEqual(result1, ['h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd']), {result: result1, message: "Test 1 No pasado "})
+if(arrayIsEqual(result1, ['h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd'])) {
+  console.log("Test 1 Pasado")
+}
 
-const result2 = split('hello,cruel,world', ',')
-console.assert(arrayIsEqual(result2, ['hello', 'cruel', 'world']), {result: result2, message: "Test 2 No pasado "})
+const result2 = split('hello,world,', ',')
+console.assert(arrayIsEqual(result2, ['hello', 'world']), {result: result2, message: "Test 2 No pasado "})
+if(arrayIsEqual(result2, ['hello', 'world'])) {
+  console.log("Test 2 Pasado")
+}
 
-const result3 = split('hello-cruel-world', '-')
-console.assert(arrayIsEqual(result3, ['hello', 'cruel', 'world']), {result: result3, message: "Test 3 No pasado "})
+const result3 = split('hello-world-', '-')
+console.assert(arrayIsEqual(result3, ['hello', 'world']), {result: result3, message: "Test 3 No pasado "})
+if (arrayIsEqual(result3, ['hello', 'world'])) {
+  console.log("Test 3 pasado")
+}
 
 
 
