3b643e47ec8fd8623ea76b9975b06d114ce697a1
xFNighTMaRe <brodriguezserrano54@gmail.com>
2024-08-01 16:37:58 +0200

    add challenge resolution #26

--- staff/ventura-rodriguez/challenges/binary-to-decimal.js
+++ staff/ventura-rodriguez/challenges/binary-to-decimal.js
@@ -3,10 +3,19 @@
 
 function binaryToDecimal(binary) {
   var decimal = 0;
+  var length = binary.length;
+
+  for (var i = 0; i < length; i++) {
+    var value = binary[length - 1 - i];
+
+    if (value === "1") {
+      decimal += Math.pow(2, i);
+    }
+  }
 
   return decimal;
 }
 
-var binary = "1110";
+var binary = "10010010"; // 128 64 32 16 8 4 2 1
 
 console.log(binary + " en decimal es: " + binaryToDecimal(binary));
