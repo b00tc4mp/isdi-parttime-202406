88886fb0b41cf16148872546555cb41bb42e810c
Rafa0297 <158144592+Rafa0297@users.noreply.github.com>
2024-08-05 00:26:44 +0200

    modify splice method #140

--- staff/rafael-infante/array-standalone/splice.js
+++ staff/rafael-infante/array-standalone/splice.js
@@ -35,39 +35,19 @@ const splice = (array, start, deleteCount) => {
   const array1 = [1, 2, 3, 4, 5, 6, 7, 8];
   const result1 = splice(array1, 4);
 
-  console.assert(arrayIsEqual(array1, [1, 2, 3, 4]), {
-    result: array1,
-    message: "Test 1.1 no pasado",
-  });
-  console.assert(arrayIsEqual(result1, [5, 6, 7, 8]), {
-    result: result1,
-    message: "Test 1.2 no pasado",
-  });
+  console.assert(arrayIsEqual(array1, [1, 2, 3, 4]), {result: array1, message: "Test 1.1 no pasado"});
+
+  console.assert(arrayIsEqual(result1, [5, 6, 7, 8]), {result: result1, message: "Test 1.2 no pasado"});
 
   const array2 = ["a", "b", "c", "d", "e", "f", "g", "h"];
   const result2 = splice(array2, 4, 2);
 
-  console.assert(arrayIsEqual(array2, ["a", "b", "c", "d", "g", "h"]), {
-    result: array2,
-    message: "Test 2.1 no pasado",
-  });
-  console.assert(arrayIsEqual(result2, ["e", "f"]), {
-    result: result2,
-    message: "Test 2.2 no pasado",
-  });
+  console.assert(arrayIsEqual(array2, ["a", "b", "c", "d", "g", "h"]), {result: array2, message: "Test 2.1 no pasado"});
+  console.assert(arrayIsEqual(result2, ["e", "f"]), {result: result2, message: "Test 2.2 no pasado"});
 
   const array3 = ["a", "b", "c", "d", "e", "f", "g", "h"];
   const result3 = splice(array2, 4, 2, 1, 2, 3, 4);
 
-  console.assert(
-    arrayIsEqual(array2, ["a", "b", "c", "d", 1, 2, 3, 4, "g", "h"]),
-    {
-      result: array3,
-      message: "Test 3.1 no pasado",
-    }
-  );
-  console.assert(arrayIsEqual(result2, ["e", "f"]), {
-    result: result3,
-    message: "Test 3.2 no pasado",
-  });
+  console.assert(arrayIsEqual(array2, ["a", "b", "c", "d", 1, 2, 3, 4, "g", "h"]), {result: array3, message: "Test 3.1 no pasado"});
+  console.assert(arrayIsEqual(result2, ["e", "f"]), {result: result3, message: "Test 3.2 no pasado"});
 }
