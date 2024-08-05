6142615b4a7017cbe3e509575a75400a1bf7ee12
xFNighTMaRe <brodriguezserrano54@gmail.com>
2024-08-05 21:42:31 +0200

    add more staff mine-seeker #98

--- staff/ventura-rodriguez/playground/mine-seeker.3/main.js
+++ staff/ventura-rodriguez/playground/mine-seeker.3/main.js
@@ -37,8 +37,7 @@
       stateButton.innerText = "Abandonar partida";
       foundMines = 0;
       totalMines = 16;
-      locateBombs(matrix);
-      locateNumbersAndBlanks(matrix);
+      prepareGame(matrix);
     } else {
       // Terminar la partida
       started = false;
@@ -56,6 +55,7 @@
     table.innerHTML = `
       ${printDivs()}
     `;
+    console.log(matrix);
   };
 
   function hasNoDuplicates(arr) {
@@ -82,81 +82,9 @@
     return concatString;
   }
 
-  function locateNumbersAndBlanks(matrix) {
-    // nuestro código para meter los números y los espacios en blanco
-    // analizar la matriz y colocar un valor dependiendo de su posición
+  function locateNumbersAndBlanks() {}
 
-    // primero seleccionaremos los espacios sin bomba
-    //
-    // propuesta del contador de si tiene algo al lado
-    // mirar las celdas adyacentes que son 8 para cada una y entonces decidir que poner
-
-    // nos acercamos a las bombas y ponemos los de alrededor
-    // después le metemos los espacios que quedan
-
-    // i = 5  --  j = 5
-
-    // recorremos la matriz y hacemos la parte del contador y ponemos o el blank o el numero correspondiente
-    // si nos encontramos una bomba hacemos "pass"
-    debugger;
-    for (let i = 0; i < matrix.length; i++) {
-      for (let j = 0; j < matrix[i].length; j++) {
-        const cell = matrix[i][j];
-
-        if (cell !== "*") {
-          // matrix[i-1][j-1] diagonal izquierda arriba
-          // matrix[i-1][j] arriba
-          // matrix[i-1][j+1] diagonal derecha arriba
-          // matrix[i+1][j-1] diagonal abajo izquierda
-          // matrix[i+1][j] abajo
-          // matrix[i+1][j+1] diagonal derecha abajo
-          // matrix[i][j-1] izquierda
-          // matrix[i][j+1] derecha
-
-          const relativePositions = [
-            [-1, -1],
-            [-1, 0],
-            [-1, 1],
-            [1, -1],
-            [1, 0],
-            [1, 1],
-            [0, -1],
-            [0, 1],
-          ];
-
-          let counter = 0;
-          for (let k = 0; k < 8; k++) {
-            if (matrix[i + relativePositions[k][0]] !== undefined) {
-              if (
-                matrix[i + relativePositions[k][0]][
-                  j + relativePositions[k][1]
-                ] === "*"
-              ) {
-                counter++;
-              }
-            }
-          }
-          matrix[i][j] = counter === 0 ? "." : counter;
-        }
-      }
-      debugger;
-      console.log(matrix);
-    }
-
-    // debugger;
-    // const bombPositions = matrix.reduce((acumulator, array, i) => {
-    //   const result = acumulator.map((element) => element !== null);
-    //   // result es mi array con los true y los false
-    //   // [true, false, true...]
-    //   return result.map((element, j) => (element === true ? [j, i - 1] : null));
-    //   // parámetros son element y j, return el if de si element es === true, en caso de que si
-    //   // retorno coordenadas en caso de que no retorno null
-    // });
-
-    console.log("hello wolrd");
-  }
-
-  function locateBombs(matrix) {
+  function prepareGame(matrix) {
     let randomNumbers = [];
     let isDuplicated = true;
 
@@ -182,10 +110,11 @@
   }
 })();
 
+// Queda por localizar en el mapa los números a la hora de preparar la partida y los espacios en blanco
+
 // EL funcionamiento va a ser que cuando hagamos click izquierdo nos pulsa sobre el mapa y desvela
 // la posición, si la posición es blanca todo ok, pondra blank o el número correcspondiente
 // la posición es bomba petará y se le sumará una perdida y se reinicia el tablero
-// si se coloca en un banl se deben mostrar todos los blanks
 
 // El funcionamiento del click derecho va a ser poner una flag
 
