77bc850c1a5d0ba205172953080bed06f3dd5c9a
CarlosF <carlosfernandezsoldado@gmail.com>
2024-08-07 19:18:39 +0200

    adding lines #119

--- staff/carlos-fernandez/playground/mine-seeker/main.js
+++ staff/carlos-fernandez/playground/mine-seeker/main.js
@@ -1,9 +1,11 @@
 const changePlayerNameButton = document.getElementById("changePlayerName");
-let displayNameSpan = document.getElementById("displayPlayerName");
-
+const displayNameSpan = document.getElementById("displayPlayerName");
+const restartButton = document.getElementById("restartButton");
+const stateButton = document.getElementById("stateButton");
+const minesFound = document.getElementById("minesFound");
+const totalMines = document.getElementById("totalMines");
 
 changePlayerNameButton.onclick = () => {
     const playerName = prompt('Please enter your name.');
     displayNameSpan.innerText = playerName;
-
 }
