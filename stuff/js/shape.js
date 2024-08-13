cfb2db0384f87ac4e34ede3c8bf2532418094e52
xFNighTMaRe <brodriguezserrano54@gmail.com>
2024-08-13 20:16:37 +0200

    add more stuff #115

--- stuff/js/shape.js
+++ stuff/js/shape.js
@@ -1,15 +1,17 @@
 function Shape() {
-  this._name = "Shape";
+  this.prototype._name = "Shape";
   this._color = "";
   this._area = 0;
   this._sidesCount = 0;
   this._perimeter = 0;
-  this.setName = function (name) {
-    this._name = name;
-  };
-  this.getName = function () {
-    return this._name;
-  };
 }
+
+Shape.prototype.setName = function (name) {
+  this._name = name;
+};
+
+Shape.prototype.getName = function () {
+  return this._name;
+};
 
 const shape1 = new Shape();
