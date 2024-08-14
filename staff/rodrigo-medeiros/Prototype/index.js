e0a688530a51a251f9e7628a934f56ed8bfe3001
rodrigo-medeiros <gapulflu83@gmail.com>
2024-08-14 19:30:09 -0300

    delete files

--- staff/rodrigo-medeiros/Prototype/index.js
+++
@@ -1,6 +0,0 @@
-const ChainCharacter = require("./contructor.js");
-const at = require("./at.js");
-
-ChainCharacter.prototype.at = at;
-
-module.exports = ChainCharacter;
--- staff/rodrigo-medeiros/Prototype/index.test.js
+++
@@ -1,1 +0,0 @@
-require("./at.test.js");
