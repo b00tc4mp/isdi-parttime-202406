38822685b43a173044327a5ba9311c0c556ad36f
CarlosF <carlosfernandezsoldado@gmail.com>
2024-08-16 20:45:24 +0200

    add includes, index-of and pop #141

--- staff/carlos-fernandez/array-standalone/index-of.js
+++ staff/carlos-fernandez/array-standalone/index-of.js
@@ -21,3 +21,24 @@ function indexOf(array, searchElement, fromIndex = 0) {
   }
   return -1;
 }
+
+const array1 = [1, 2, 3, 4, 5];
+const result1 = indexOf(array1, 2, 0);
+console.assert(result1 === array1.indexOf(2, 0), {
+  result: result1,
+  message: "Test 1 No pasado",
+});
+
+const array2 = ["Como", "estan", "los", "máquinas"];
+const result2 = indexOf(array2, "máquinas", 2);
+console.assert(result2 === array2.indexOf("máquinas", 2), {
+  result: result2,
+  message: "Test 2 No pasado",
+});
+
+const array3 = ["lo", 3, "primero", 2, "todo"];
+const result3 = indexOf(array3, 2, -1);
+console.assert(result3 === array3.indexOf(2, -1), {
+  result: result3,
+  message: "Test 3 No pasado",
+});
