62accef424ef22f584b9fc527e2de50a824b659b
xFNighTMaRe <brodriguezserrano54@gmail.com>
2024-08-27 21:11:00 +0200

    add heritage exaple #115

---
+++ stuff/js/class.js
@@ -0,0 +1,36 @@
+class Persona {
+  // función constructora o esquema cosntructor
+  constructor(nombre, edad) {
+    this.nombre = nombre;
+    this.edad = edad;
+  }
+
+  // esta función es un método del esquema persona, se adhiere a su prototipo
+  saludar() {
+    console.log(`Hola, me llamo ${this.nombre} y tengo ${this.edad} años.`);
+  }
+}
+
+//
+const hommer = new Persona("Hommer", 37);
+hommer.saludar();
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
+class Bombero extends Persona {
+  constructor(nombre, edad, estacion) {
+    // Llama al constructor de la clase padre (Persona)
+    super(nombre, edad);
+    this.estacion = estacion;
+  }
+
+  apagarIncendio() {
+    console.log(
+      `${this.nombre} está apagando un incendio desde la estación ${this.estacion}.`
+    );
+  }
+}
+
+const paco = new Bombero("Paco", 27, "Barcelona Sants");
+paco.saludar();
+paco.apagarIncendio();
