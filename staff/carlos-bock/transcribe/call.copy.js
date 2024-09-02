9fff34673658331580dcbb0f444c6182a4540e77
carlos <carlosrbock@gmail.comgit>
2024-09-02 12:04:35 +0200

    add files #133

---
+++ staff/carlos-bock/transcribe/call.copy.js
@@ -0,0 +1,11 @@
+function saludar(saludo, signo) {
+    console.log(`${saludo}, ${this.nombre}${signo}`);
+  }
+  
+  const persona = { nombre: "Juan" };
+  
+  saludar.call(persona, "Hola", "!"); // Output: "Hola, Juan!"
+  
+  const persona2 = { nombre: "Limon" };
+  
+  saludar.call(persona2, "Heeeeey", "😎");
