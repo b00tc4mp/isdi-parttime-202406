653b2ba7e1c3880b379eb02b9b67875939121fdd
xFNighTMaRe <brodriguezserrano54@gmail.com>
2024-09-03 19:12:20 +0200

    revert last commit

--- staff/angela-bernaldez/chain-characters/at.js
+++ staff/angela-bernaldez/chain-characters/at.js
@@ -1,28 +1,24 @@
-const ChainCharacters = require("./constructor.js")
+const ChainCharacters = require("./constructor.js");
 
+function at(index) {
+  if (index === null) return new ChainCharacters(this.value[0]);
 
-  
-    if (index === null) return new ChainCharacters(this.value[0]);
-  
-    let result;
-  
-    for (let i = 0; i < this.length; i++) {
-      const character = this.value[i];
-  
-      if (i === index) result = character;
-      else if (index < 0 && i === this.length + index) result = character
-    }
+  let result;
 
-    // return a new instance of the constructor function, initialized with the result string
-    // this means that instead of modifying the original object, 
-    // the method generates a fresh ChainCharacters object that contains the repeated string. 
+  for (let i = 0; i < this.length; i++) {
+    const character = this.value[i];
 
-    // if result was returned, a string would be returned in this case
-    // and we want to return the same type of data - object 
-    return new ChainCharacters(result)
+    if (i === index) result = character;
+    else if (index < 0 && i === this.length + index) result = character;
+  }
+
+  // return a new instance of the constructor function, initialized with the result string
+  // this means that instead of modifying the original object,
+  // the method generates a fresh ChainCharacters object that contains the repeated string.
+
+  // if result was returned, a string would be returned in this case
+  // and we want to return the same type of data - object
+  return new ChainCharacters(result);
 }
 
-module.exports = at
-  
-
-  
+module.exports = at;
