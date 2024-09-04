60f4cd0752f47f08d9deb4afe199ab8e35819209
CarlosF <carlosfernandezsoldado@gmail.com>
2024-09-04 16:28:32 +0200

    fixing split #192

--- staff/carlos-fernandez/chain-characters/split.js
+++ staff/carlos-fernandez/chain-characters/split.js
@@ -1,84 +1,36 @@
-const ChainCharacters = require("./constructor");
+const ChainCharacters = require("./constructor.js");
 
-function split(separator, limit) {
-  let newArray = [];
-  let newString = "";
+function split(separator, limit = undefined) {
+  let result = [];
+  let currentSegment = "";
+  // counter = contador de elementos del array. Cuando counter > limit-1, significa que hemos llegado al límite de elementos
+  let counter = 0;
   let i = 0;
 
-  // Caso especial cuando el separador es una cadena vacía
-  if (separator === "") {
-    while (i < this.length) {
-      newArray[newArray.length] = this.value[i];
-      i++;
+  while ((i < this.length) | (counter > limit - 1)) {
+    if (separator === "") {
+      result[result.length] = this.value[i];
     }
-    if (limit !== undefined) return newArray.slice(0, limit); // Limitar si es necesario
-    return newArray;
-  }
+    if (this.value[i] === separator) {
+      result[result.length] = currentSegment;
+      currentSegment = "";
+      counter++;
 
-  while (i < this.length) {
-    let match = true;
-
-    // Comprobar si el separador coincide
-    for (let j = 0; j < separator.length; j++) {
-      if (this.value[i + j] !== separator[j]) {
-        match = false;
+      if (limit !== undefined && counter > limit - 1) {
         break;
       }
-    }
-
-    // Si encontramos una coincidencia
-    if (match) {
-      newArray[newArray.length] = newString;
-      newString = "";
-      i += separator.length; // Saltar la longitud del separador
     } else {
-      newString += this.value[i];
-      i++;
+      currentSegment += this.value[i];
     }
+    i++;
   }
 
-  // Añadir la última parte
-  newArray[newArray.length] = newString;
-
-  // Aplicar el límite si es necesario
-  if (limit !== undefined) {
-    return newArray.slice(0, limit);
+  // Add the last segment if it hasn't exceeded the limit
+  if (limit === undefined || counter < limit) {
+    result[result.length] = currentSegment;
   }
 
-  return new ChainCharacters(newArray);
+  return new ChainCharacters(result);
 }
 
 module.exports = split;
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// EXPLICACION DEL CÓDIGO
-
-/*
-    Esta función agarra el string y lo corta en cuanto encuentra una coincidencia con separator. 
-    Además, lo corta en tantas partes como index haya.
-
-    Para ello, hay que:
-        1) Crear una array vacía.
-        2) Iterar el string. Si NO coincide con separator, añadirlo a esta nueva array creada anteriormente en forma de string. 
-        3) Toda coincidencia entre string y separator no puede aparecer en la nueva array. 
-        4) Lo que venga detrás de esa coincidencia, añadirlo a una nueva string dentro de la array anteriormente creada. 
-        5) El tercer parámetro de esta función (limit) nos indicará en cuántas strings queremos separar la array. Si le hemos dicho que nos separe el 
-           string por espacios (' ') y le hemos puesto el límite en 2, nos devolverá las 2 primeras palabras. Si le hemos dicho que nos separe el string
-           por carácteres ('') con limit =2, nos devolverá los 2 primeros carácteres. 
-
-        Si limit = null: devolverá un array con único string.
-        Si separator no existe: devolverá un array con único string.
-        Si limit = 0: devolverá un array vacío.
-        Si limit > que los posibles cortes, limit será igual al máximo de cortes que se puedan hacer. 
-
-        EJEMPLO: 
-
-        'Si saben como me pongo pa que me invitan'.split(' ', 25)
-        El límite es 9, porque al separarlo por espacios, sólo se puede separar en 9 palabras. Al darle un limit = 25, como máximo lo separará en 9.
-        
-*/
-
-// str = string sobre la que trabajaremos
-// separator = string sobre la que vamos a separar la main string
-// limit = límite de strings que va a devolver en el array
--- staff/carlos-fernandez/chain-characters/split.test.js
+++ staff/carlos-fernandez/chain-characters/split.test.js
@@ -1,31 +1,52 @@
 const ChainCharacters = require(".");
 
-function arraysEqual(arr1, arr2) {
-  if (arr1.length !== arr2.length) return false;
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) return false;
+const paragraph = "Quasimodo, Flora";
+const result1 = new ChainCharacters(paragraph).split(",");
+console.assert(
+  result1.value[0] === "Quasimodo" && result1.value[1] === " Flora",
+  {
+    result: result1.value,
+    message: "Test 1 No pasado",
   }
-  return true;
-}
+);
 
-const result1 = new ChainCharacters("Deep house techno").split("De");
-const expected1 = "Deep house techno".split("De");
-console.assert(arraysEqual(result1, expected1), {
-  result: result1,
-  message: "Test 1 no pasado",
-});
-console.log(result1);
+const result2 = new ChainCharacters("Tini").split("");
+console.assert(
+  result2.value[0] === "T" &&
+    result2.value[1] === "i" &&
+    result2.value[2] === "n" &&
+    result2.value[3] === "i",
+  {
+    result: result2.value,
+    message: "Test 2 No pasado",
+  }
+);
 
-const result2 = new ChainCharacters("El detectiu Conan").split("");
-const expected2 = "El detectiu Conan".split("");
-console.assert(arraysEqual(result2, expected2), {
-  result: result2,
-  message: "Test 2 no pasado",
-});
+const result3 = new ChainCharacters("1 2 3 4 5 6").split(" ", 3);
+console.assert(
+  result3.value[0] === "1" &&
+    result3.value[1] === "2" &&
+    result3.value[2] === "3",
+  {
+    result: result3.value,
+    message: "Test 3 No pasado",
+  }
+);
 
-const result3 = new ChainCharacters("Shinosuke Nohara").split(" ");
-const expected3 = "Shinosuke Nohara".split(" ");
-console.assert(arraysEqual(result3, expected3), {
-  result: result3,
-  message: "Test 3 no pasado",
+const result4 = new ChainCharacters("2024-07-20").split("-", 3);
+console.assert(
+  result4.value[0] === "2024" &&
+    result4.value[1] === "07" &&
+    result4.value[2] === "20" &&
+    result4.value[3] === undefined,
+  {
+    result: result4.value,
+    message: "Test 4 No pasado",
+  }
+);
+
+const result5 = new ChainCharacters("2024-07-20").split(" ", 3);
+console.assert(result5.value[0] === "2024-07-20", {
+  result: result5.value,
+  message: "Test 5 No pasado",
 });
