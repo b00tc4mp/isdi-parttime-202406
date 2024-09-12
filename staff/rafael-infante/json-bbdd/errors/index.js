bac28200b6f652e88c0691ef4dd27824a6cc7b45
Rafa0297 <158144592+Rafa0297@users.noreply.github.com>
2024-09-12 22:04:19 +0200

    validate id number #213

--- staff/rafael-infante/json-bbdd/errors/index.js
+++ staff/rafael-infante/json-bbdd/errors/index.js
@@ -1,7 +1,9 @@
 const NotFoundError = require('./not-found.js');
 const CredentialsError = require('./credentials.js');
+const NotAnIntegerError = require('./not-an-integer.js');
 
 module.exports = {
   CredentialsError,
   NotFoundError,
+  NotAnIntegerError,
 }
--- staff/rafael-infante/json-bbdd/scripts/users/update-by-id.js
+++ staff/rafael-infante/json-bbdd/scripts/users/update-by-id.js
@@ -1,11 +1,14 @@
 const fs = require('fs');
 const path = require('path');
-const read = require('./read-all.js')
+const read = require('./read-all.js');
+const NotAnIntegerError = require('../../errors/not-an-integer.js');
 
 function updateById(id, data) {
-  if (typeof id !== 'number') throw new TypeError('id is not a number');
 
-  const {name, birth_date, phone} = data;
+  if (typeof id !== 'number' || Number.isNaN(id)) throw new TypeError('id is not a number');
+  if (!Number.isInteger(id)) throw new NotAnIntegerError('id is not an integer number');
+
+  const { name, birth_date, phone } = data;
   read((users) => {
     users.forEach((user) => {
       if (user.id === id) {
@@ -14,10 +17,10 @@ function updateById(id, data) {
         user.phone = phone ?? user.phone;
       }
     });
-
+    if (id >= users.length) throw new RangeError('id not found')
     fs.writeFile(
-      path.join(__dirname,"/../../bbdd/users.json"),
-      JSON.stringify({ users: users}),
+      path.join(__dirname, "/../../bbdd/users.json"),
+      JSON.stringify({ users: users }),
       'utf-8',
       (err) => {
         if (err) throw err;
