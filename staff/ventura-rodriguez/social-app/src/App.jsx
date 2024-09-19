e5b6c379e107d7f88bf6abbf79e957f3079e97a0
xFNighTMaRe <brodriguezserrano54@gmail.com>
2024-09-19 21:56:05 +0200

    add landing to social app #220

---
+++ staff/ventura-rodriguez/social-app/src/App.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import pages from "./pages/index.jsx";
+const { Landing } = pages;
+
+function App() {
+  return (
+    <main className="App">
+      <Landing />
+      {/* 
+        - Landing
+        - Login
+        - Registro
+        - Home
+        - Feed
+        - Create content
+        - Profile (view)
+        - Profile (edit)
+        - People
+        - Explorer
+        - Settings
+        - Conversations (list)
+        - Conversations (only one)
+        - Notificacions
+      */}
+    </main>
+  );
+}
+
+export default App;
---
+++ staff/ventura-rodriguez/social-app/src/pages/index.jsx
@@ -0,0 +1,7 @@
+import Landing from "./Landing.jsx";
+
+const index = {
+  Landing
+}
+
+export default index
