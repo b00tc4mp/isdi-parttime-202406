9958daa806077aec8eaa2f5e0e825c1a26336da4
CarlosF <carlosfernandezsoldado@gmail.com>
2024-09-21 16:42:03 +0200

    find.js #141

--- staff/carlos-fernandez/array-standalone/find.js
+++ staff/carlos-fernandez/array-standalone/find.js
@@ -1,7 +1,7 @@
 // Devuelve el ÍNDICE del PRIMER ELEMENTO de un array que cumpla con la función aportada.
 // De no ser así, devuelve -1.
 
-function findIndex(array, callback) {
+function find(array, callback) {
   for (let i = 0; i < array.length; i++) {
     const element = array[i];
 
@@ -11,14 +11,14 @@ function findIndex(array, callback) {
 }
 
 const array1 = [1, 2, 3, 4, 5];
-const result1 = findIndex(array1, (element) => element > 4);
+const result1 = find(array1, (element) => element > 4);
 console.assert(result1 === array1.findIndex((element) => element > 4), {
   result: result1,
   message: "Test 1 No pasado",
 });
 
 const array2 = ["a", 20, "a", 40, -1];
-const result2 = findIndex(array2, (item) => typeof item === "number");
+const result2 = find(array2, (item) => typeof item === "number");
 console.assert(
   result2 === array2.findIndex((item) => typeof item === "number"),
   {
@@ -28,7 +28,7 @@ console.assert(
 );
 
 const array3 = [1, 2, 3, 4, 5];
-const result3 = findIndex(array3, (item) => typeof item !== "number");
+const result3 = find(array3, (item) => typeof item !== "number");
 console.assert(
   result3 === array3.findIndex((item) => typeof item !== "number"),
   {
