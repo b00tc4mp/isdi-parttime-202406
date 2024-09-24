96da43711274df4ae9dcf4558b93792b7c0a80eb
rodrigo-medeiros <gapulflu83@gmail.com>
2024-09-24 17:38:27 -0300

    adding more code

---
+++ staff/rodrigo-medeiros/social-app/src/pages/Landing.jsx
@@ -0,0 +1,25 @@
+import { Component } from "react";
+
+class Landing extends Component {
+  render() {
+    return (
+      <section className="w-full h-full min-w-[100vw] min-h-[100vh] grid">
+        <div className="place-self-center w-fit">
+          <div className="w-fit mx-auto mb-9">
+            <h1 className="text-3xl">Bienvenido a mi Social App</h1>
+          </div>
+          <div className="grid grid-cols-2 gap-8">
+            <button className="btn btn-primary btn-md">
+              Ir al formulario de registro
+            </button>
+            <button className="btn btn-secondary btn-md">
+              Ir al formulario de entrada
+            </button>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Landing;
