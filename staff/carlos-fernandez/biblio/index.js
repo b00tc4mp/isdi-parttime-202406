dae5db2e09dddb671b2ac6b83a136fe9d313c166
CarlosF <carlosfernandezsoldado@gmail.com>
2024-10-14 20:12:22 +0200

    biblio_index #192

--- staff/carlos-fernandez/biblio/index.js
+++ staff/carlos-fernandez/biblio/index.js
@@ -3,13 +3,13 @@ const Biblio = require("./constructor.js");
 const at = require("./at.js");
 const concat = require("./concat.js");
 const copyWithin = require("./copy-within.js");
-//const every = require("./every.js");
+const every = require("./every.js");
 const fill = require("./fill.js");
 
 Biblio.prototype.at = at;
 Biblio.prototype.concat = concat;
 Biblio.prototype.copyWithin = copyWithin;
-//Biblio.prototype.every = every;
+Biblio.prototype.every = every;
 Biblio.prototype.fill = fill;
 
 module.exports = Biblio;
