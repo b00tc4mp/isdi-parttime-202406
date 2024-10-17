6bef87b1bf337b02c61c1d2e5cd79db68939946a
Angela Bernaldez <angelabernaldz@gmail.com>
2024-10-17 19:43:44 +0100

    add prototype exercises #194

--- staff/angela-bernaldez/biblio/slice.js
+++ staff/angela-bernaldez/biblio/slice.js
@@ -7,8 +7,6 @@ function slice(start = 0, end = this.length) {
     start = start < 0 ? Math.max(start + this.length, 0) : start;
     end = end < 0 ? end + this.length : Math.min(end, this.length); 
 
-    // Return an empty array if start is greater than or equal to end
-    // In contrast to fill method - where the original array was returned when start exceeded end
     if (start >= end) return newArray
 
     for (let i = start; i < end; i++) {
