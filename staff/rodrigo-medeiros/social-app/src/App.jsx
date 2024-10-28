ee3f858af8b4a35282f8dc0d9f5c165c0f5eadde
rodrigo-medeiros <gapulflu83@gmail.com>
2024-10-28 16:57:57 -0300

    add more things do social app

--- staff/rodrigo-medeiros/social-app/src/App.jsx
+++ staff/rodrigo-medeiros/social-app/src/App.jsx
@@ -1,24 +1,19 @@
 import React from "react";
-import { Landing, Login, SignOut, Page404, SignUp } from "./pages";
+import { Landing, Login, Signup, Page404, Home } from "./pages";
 import { Route, Routes, Navigate } from "react-router-dom";
-
 
 function App() {
   return (
     <main className="App">
-      
-        <Routes>
+      <Routes>
         <Route path="/" element={<Landing />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/sign-up" element={<SignUp />} />
-          <Route path="/page404" element={<Page404 />} />
-          <Route path="/sign-out" element={<SignOut />} /> 
-          <Route path="*" element={<Navigate to="/Page404" />} />
-        </Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/not-found" element={<Page404 />} />
+        <Route path="*" element={<Navigate to="/not-found" />} />
+      </Routes>
       {/* 
-        - Landing
-        - Login
-        - Registro
         - Home
         - Feed
         - Create content
