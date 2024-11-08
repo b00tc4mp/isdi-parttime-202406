b6bc8ddf8201e00c1486a1ed5ac82ff94996383e
Flors Rueda <ruedaflors@gmail.com>
2024-11-08 19:31:57 +0100

    refactor registeruser in api #247

--- staff/flors-rueda/social-api/index.js
+++ staff/flors-rueda/social-api/index.js
@@ -3,46 +3,14 @@
 
 import express, { json } from 'express';
 import fs from 'fs';
+import handlers from './handlers/index.js';
 
 const server = express();
 const port = 4321;
 
 const jsonBodyParser = json();
 
-
-server.get('/', (req, res) => {
-    res.send('hola mundo');
-})
-
-// index.js --> server.get("/users", jsonBodyParser, (req, res) => handlers.registerUser(req, res))
-
-//handlers --> registerUser(req, res) => { const {}}
-
-
-
-server.post("/users", jsonBodyParser, async (req, res) => {
-    try {
-        const data = await fs.readFile(path.join(__dirname, './db/users.json'), 'utf-8');
-        let users = []
-        if (data) {
-            users = JSON.parse(data);
-        }
-        // res.send(users);
-        const { username, password } = req.body // esto se manejaria en el handlers
-        console.log(users)
-        users.push({ username, password })
-
-        fs.writeFile(
-            path.join(__dirname, './db/users.json'),
-            JSON.stringify(users),
-            'utf-8'
-        );
-        res.send("user registered correctly") //iria en el handlers
-    } catch (err) {
-        console.error('Error al obtener usuarios:', err);
-        res.status(500).send('Error interno del servidor');
-    }
-})
+server.post('/users', jsonBodyParser, (req, res) => handlers.registerUser(req, res))
 
 server.get('/users', async (req, res) => {
     try {
@@ -55,8 +23,10 @@ server.get('/users', async (req, res) => {
     } catch (err) {
         console.error('Error al obtener usuarios:', err);
         res.status(500).send('Error interno del servidor');
-    }
+    }  // TODO: Separar por capas este metodo
 });
+
+//Añadir un put, un delete, patch username, patch email, patch password
 
 server.listen(port, () => {
     console.log(`Server running on port:`, port)
