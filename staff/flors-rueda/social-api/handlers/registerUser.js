22036a468f10f8cf5161da20f73bae13a0cce199
Flors Rueda <ruedaflors@gmail.com>
2024-11-11 21:37:23 +0100

    add spoilers in comments #247

--- staff/flors-rueda/social-api/handlers/registerUser.js
+++ staff/flors-rueda/social-api/handlers/registerUser.js
@@ -1,4 +1,5 @@
 import logic from "../logic/index.js"
+// import Errors from "social-com"
 
 export default (req, res) => {
     const { username, dateOfBirth, email, password } = req.body
@@ -8,6 +9,7 @@ export default (req, res) => {
 
         res.status(201).send();
     } catch (error) {
+        // if(error.isntaceOf(Error.DuplicationError))
         if (error.message === "Username already in use" || error.message === "Email already in use") {
             res.status(409).send(error.message)
         } else {
--- staff/flors-rueda/social-api/logic/registerUser.js
+++ staff/flors-rueda/social-api/logic/registerUser.js
@@ -1,10 +1,12 @@
 import storage from '../db/sync-storage.js'
+//import Errors from 'social-common'
 
 export default (username, dateOfBirth, email, password) => {
     //TODO validar tipo de input, regex, etc ---> si falla lanzar error
 
     const userNameDuplicated = storage.users.some((user) => user.username === username);
     if (userNameDuplicated) throw new Error("Username already in use");
+    // throw new Errors.DuplicationError("Username already in use");
 
     const userEmailDuplicated = storage.users.some((user) => user.email === email);
     if (userEmailDuplicated) throw new Error("Email already in use");
