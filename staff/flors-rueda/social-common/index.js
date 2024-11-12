211d9487bbf204bba665d53a93440bc641802529
Flors Rueda <ruedaflors@gmail.com>
2024-11-12 13:33:33 +0100

    add initial version of social-common folder #247

---
+++ staff/flors-rueda/social-common/index.js
@@ -0,0 +1,4 @@
+export * as Errors from "./errors"; //Exportamos todo lo que hay en errors.js como Errors
+// eslint-disable-next-line import/first
+import _Validator from "./validator.js"; // Nos traemos lo que hay en validator.js como _Validator 
+export const Validator = _Validator; // Lo exportamos como Validator
