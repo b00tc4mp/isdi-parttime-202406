18335bca5d1c8565317f4bf1978c64e33dde7382
Angela Bernaldez <angelabernaldz@gmail.com>
2024-11-24 13:59:03 +0000

    add first async logic (register user) #223

--- staff/angela-bernaldez/social-api/handlers/registerUser.js
+++ staff/angela-bernaldez/social-api/handlers/registerUser.js
@@ -4,9 +4,18 @@ export default (req, res, next) => {
     const { username, 'date-of-birth': dateOfBirth, email, password } = req.body
 
     try {
-        logic.registerUser(username, dateOfBirth, email, password);
+        /* SYNC PROCESS */
+        logic.registerUser(username, dateOfBirth, email, password)
 
-        res.status(201).send();
+        res.status(201).send()
+
+
+        //ASYNC PROCESS
+        /*
+        logic.registerUser(username, dateOfBirth, email, password)
+            .then(() => res.status(201).send())
+            .catch(error => next(error))
+        */
     } catch (error) {
         next(error)
     }
--- staff/angela-bernaldez/social-api/logic/registerUser.js
+++ staff/angela-bernaldez/social-api/logic/registerUser.js
@@ -1,4 +1,4 @@
-import storage from '../db/sync-storage.js'
+import storage from '../db/async-storage.js'
 import { Validator, Errors } from 'social-common'
 
 export default (username, dateOfBirth, email, password) => {
@@ -8,20 +8,21 @@ export default (username, dateOfBirth, email, password) => {
     Validator.email(email)
     Validator.password(password)
 
-    // then/catch (promises)
-    const userNameDuplicated = storage.users.some((user) => user.username === username)
-    // if (userNameDuplicated) throw new Errors.DuplicityError("Username already in use")
-
-    const userEmailDuplicated = storage.users.some((user) => user.email === email);
-    if (userEmailDuplicated) throw new Errors.DuplicityError("Email already in use")
-
-    const user = {
-        id: Date.now(),
-        username,
-        dateOfBirth,
-        email,
-        password
-    }
-
-    storage.users = user;
+    return storage.getUsers()
+        .then(users => {
+            if (users.some(user => user.username === username)) {
+                throw new Errors.DuplicityError("Username already in use")
+            }
+            if (users.some(user => user.email === email)) {
+                throw new Errors.DuplicityError("Email already in use")
+            }
+            const user = {
+                id: Date.now(),
+                username,
+                dateOfBirth,
+                email,
+                password
+            }
+            return storage.addUser(user);
+        })
 }
