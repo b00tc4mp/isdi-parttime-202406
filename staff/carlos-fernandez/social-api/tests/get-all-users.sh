e6c89de9bfc82de3b24645d212c02e5e1dbc1849
CarlosF <carlosfernandezsoldado@gmail.com>
2024-12-01 18:46:53 +0100

    tests.sh, create-comment, get-all-public-posts, toggle-like #222

--- staff/carlos-fernandez/social-api/tests/get-all-users.sh
+++ staff/carlos-fernandez/social-api/tests/get-all-users.sh
@@ -1,2 +1,2 @@
 curl -X GET http://localhost:4321/users \
-    -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjE3MzE1MDMzMjQ2NzQiLCJpYXQiOjE3MzE1MjYwMjB9.dLjJn9ajRdQkFQu9LD82lDx3Gq_GCS5vzIPhhWuEPbs" -v
+    -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY3NDczMGVmYmI2MzVhMzJlMzQzMjI3NSIsImlhdCI6MTczMjcxOTE1N30.Eth6fK03GHr7O1Rp61d0em9q4sZbqRmrAd7TlDeHipQ" -v
---
+++ staff/carlos-fernandez/social-api/tests/toggle-like.sh
@@ -0,0 +1,3 @@
+curl -X PATCH http://localhost:4321/posts/67473fa7e1e7b275e61f87c5 \
+    -H "Content-Type: application/json" \
+    -H "Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY3NDczZmE3ZTFlN2IyNzVlNjFmODdhZSIsImlhdCI6MTczMjczNDc0OX0.gfZ8heHXpFIWEqwLXKfNiCkkmdXcJtBVB19bRdRusu4" -v
