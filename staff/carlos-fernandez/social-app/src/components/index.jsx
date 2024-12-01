a9f3a786acda65821db96a54550824780c7d1c04
CarlosF <carlosfernandezsoldado@gmail.com>
2024-12-01 18:52:22 +0100

    components. PostCard and Comment, fix Header #222

--- staff/carlos-fernandez/social-app/src/components/index.jsx
+++ staff/carlos-fernandez/social-app/src/components/index.jsx
@@ -3,7 +3,7 @@ import _Footer from "./Footer.jsx";
 import _FormErrorsSection from "./FormErrorsSection.jsx";
 import _Header from "./Header.jsx";
 import _LoginForm from "./LoginForm.jsx";
-import _SignupForm from "./SignupForm.jsx";
+import _SignupForm from "./SignUpForm.jsx";
 
 export const DatePicker = _DatePicker;
 export const Footer = _Footer;
