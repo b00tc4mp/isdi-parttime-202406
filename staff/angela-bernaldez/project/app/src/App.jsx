67c73a999eb2961a0274fcfc7ccd8e5f567464e4
Angela Bernaldez <angelabernaldz@gmail.com>
2024-12-08 21:11:15 +0000

    add weather app first steps #251

---
+++ staff/angela-bernaldez/project/app/src/App.jsx
@@ -0,0 +1,28 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+        <h1>Vite + React</h1>
+        <h1>My Weather App</h1>
+      <div className="card">
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
