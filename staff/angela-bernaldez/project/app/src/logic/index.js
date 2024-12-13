dbac065c3756e3bd1072a6ad7b2212d03d908b8f
Angela Bernaldez <angelabernaldz@gmail.com>
2024-12-13 20:16:04 +0000

    add registerUser logic app #251

---
+++ staff/angela-bernaldez/project/app/src/logic/index.js
@@ -0,0 +1,5 @@
+import registerUser from './registerUser.js'
+
+export default {
+    registerUser
+}
---
+++ staff/angela-bernaldez/project/app/src/logic/registerUser.js
@@ -0,0 +1,32 @@
+import { Errors } from 'common'
+
+const registerUser = ({ username, email, password, repeatPassword}) => {
+    // TODO: add validators
+
+    const user = {
+        username,
+        email,
+        password
+    }
+
+    return fetch(`${process.env.VITE_API_URL}users`, {
+        method: 'POST',
+        headers: {
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify(user)
+    })
+    .then((res) => {
+        if (res.status === 201) return
+        return res.json()
+        .then(body => {
+            const constructor = Errors[body.name]
+            throw new constructor(`${body.message}`)
+        })
+    })
+    .catch((error) => {
+        if (error instanceof TypeError)
+          throw new Errors.ServerError('Server in not connected')
+        throw error
+    })
+}
