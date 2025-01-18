8bd04be30ee957d4c3a75d5a9c7f0892005e9d81
CarlosF <carlosfernandezsoldado@gmail.com>
2025-01-18 15:31:04 +0100

    getUserDogs done #255

--- staff/carlos-fernandez/project/api/tests/get-user-dogs.sh
+++ staff/carlos-fernandez/project/api/tests/get-user-dogs.sh
@@ -1,11 +1,7 @@
-#!/bin/bash
-
-# Variables de entorno
-API_URL="http://localhost:4444"
 TOKEN="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY3NWZkZjNmMmI3MWE0ODYwYmMyNTBjNyIsImlhdCI6MTczNzE5Njc5NH0.eE_pSPuIKLfKqgwK8hI2XI_oMB_YfA8OOqzehUmrndw"
-USER_ID="675fdf3f2b71a4860bc250c7"  # Reemplaza con un ID de usuario válido
+USER_ID="675fdf3f2b71a4860bc250c7"
 
 # Comando curl para obtener los perros del usuario
-curl -X GET "${API_URL}/users/${USER_ID}/dogs" \
+curl -X GET "http://localhost:4444/users/${USER_ID}/dogs" \
   -H "Authorization: Bearer ${TOKEN}" \
   -v
