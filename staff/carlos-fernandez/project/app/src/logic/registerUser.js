4acea3fd807debfcce680e9bbcbb385bb62bc7af
CarlosF <carlosfernandezsoldado@gmail.com>
2025-04-02 19:17:29 +0200

    registeruser #255

--- staff/carlos-fernandez/project/app/src/logic/registerUser.js
+++ staff/carlos-fernandez/project/app/src/logic/registerUser.js
@@ -38,8 +38,7 @@ const registerUser = ({
       if (res.status === 201) return;
       return res.json().then((body) => {
         const constructor = Errors[body.name];
-        console.log("RESPUESTA", res.status);
-        console.log("CONSTRUCTOR", constructor);
+
         if (res.status === 409 && constructor === Errors.DuplicityError) {
           throw new Errors.DuplicityError(body.message);
         }
